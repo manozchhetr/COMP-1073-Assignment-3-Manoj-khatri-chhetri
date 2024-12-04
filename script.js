009cf03b845707f342e41018779ba3e1f8cab954
manozchhetri <156447784+manozchhetri@users.noreply.github.com>
2024-12-03 23:08:44 -0500

    Add base API key import and element references

---
+++ script.js
@@ -0,0 +1,7 @@
+// Import API Key
+const API_KEY = "live_cbOTlBi4Hzeq8Dy0UeNI7QPnIcwUlQTvWlFnmtZftkBzn8N33d9WaMO3sD4NCz5i"; // api-key.js exports this value
+
+const breedSelector = document.getElementById('breedSelector');
+const getCatButton = document.getElementById('getCat');
+const catInfoDiv = document.getElementById('catInfo');
+const loadingDiv = document.getElementById('loading');
